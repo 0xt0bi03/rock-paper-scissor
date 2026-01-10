3d523fb708586a8a4afb125c5c792e16fb6ee5e8
Tobi_03 <m4sk3d.t0bi@gmail.com>
2026-01-10 23:26:56 +0530

    made changes

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// // Random integer between min (inclusive) and max (inclusive)
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// let compscore = 0; let humanscore = 0;
-
-// // function game()
-// // {
-// //   console.log(`Enter a number b/w 1, 2 & 3: `);
-
-// // }
-
-
-// // while (compscore < 10 && humanscore < 10)
-// // {
-// // }
 
 
 const startBtn = document.getElementById("startBtn");
